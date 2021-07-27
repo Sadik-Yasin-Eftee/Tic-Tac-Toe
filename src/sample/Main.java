1038292fb098bb0ab60fa9f0ebe4404f75ee6d8e
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2021-07-28 04:00:32 +0600

    Updated Controller

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,18 +1,42 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import java.awt.*;
+import java.lang.reflect.Array;
 
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) {
         try {
             AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("TicTacToe.fxml"));
             Scene scene = new Scene(root,650, 650);
+            //scene.lookup("#1");
+            //Button button1 = ((Button)scene.lookup("#0"));
+            //Button button = new Button();
+            //button1.setText("Changed");
+            //System.out.println(button1.getId()); // shows the index number of button
+            /*
+            int array[] = new int[9];
+            for (int i = 0 ;i < 9 ; i = i + 2){
+                String j = Integer.toString(i);
+                button = ((Button) scene.lookup("#" + j));
+                String index = button.getId();
+                int ind = Integer.parseInt(index);
+                array[i] = ind;
+                System.out.println(array[i]);
+                button.setText("O");
+
+            }
+
+             */
             //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
--- src/sample/TicTacToeController.java
+++ src/sample/TicTacToeController.java
@@ -8,26 +8,48 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.awt.*;
 import java.awt.event.ActionListener;
+import java.lang.invoke.MethodHandles;
+import java.util.Random;
 import java.util.ResourceBundle;
 
 public class TicTacToeController {
 
+
     private boolean player = true;
+
+    private boolean buttonArray[] = new boolean[9];
+
+    public void setButtonArray(boolean[] buttonArray) {
+        for (int i = 0 ; i < 9 ; i++){
+            this.buttonArray[i] = false;
+        }
+    }
+
+    public int max = 8 , min = 0;
 
     public void buttonClicked(ActionEvent actionEvent) {
         Button clickedButton = (Button) actionEvent.getTarget();
         String buttonLabel = clickedButton.getText();
         if ("".equals(buttonLabel) && player){
             clickedButton.setText("X");
+            System.out.println(clickedButton.getId());
+            this.buttonArray[Integer.parseInt(clickedButton.getId())] = true;
+            System.out.println(this.buttonArray[Integer.parseInt(clickedButton.getId())]);
             player=false;
         }
         else if ("".equals(buttonLabel) && !player) {
+            //Random randomNumber = new Random();
+            int randomNumber = (int) (Math.random()*(max - min + 1) + min);
+            System.out.println(randomNumber);
+            //if ()
             clickedButton.setText("O");
+            System.out.println(clickedButton.getId());
             player = true;
         }
         //System.out.println("Button 1");
