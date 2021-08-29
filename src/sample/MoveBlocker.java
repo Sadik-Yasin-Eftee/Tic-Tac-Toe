78e510eb79841decb8bca209b4213afa05719bf8
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2021-08-30 01:48:18 +0600

    Completed Defensive AI #13 and refactored #1

--- src/sample/MoveBlocker.java
+++ src/sample/MoveBlocker.java
@@ -4,7 +4,9 @@ import javafx.scene.text.Font;
 
 public class MoveBlocker {
     public int blockMoves() {
+
         //row check
+
         for (int i = 0; i < 9; i = i + 3) {
             if ((Main.buttons[i].getText() == "X") && (Main.buttons[i + 1].getText() == "X")) {
                 if (Main.buttons[i + 2].getText() == "") {
@@ -30,6 +32,7 @@ public class MoveBlocker {
         }
 
         //column check
+
         for (int i = 0; i < 3; i++) {
             if ((Main.buttons[i].getText() == "X") && (Main.buttons[i + 3].getText() == "X")) {
                 if (Main.buttons[i + 6].getText() == "") {
@@ -55,28 +58,25 @@ public class MoveBlocker {
         }
 
         //diagonal check
-        while (true){
-            if ((Main.buttons[0].getText() == "X") && (Main.buttons[4].getText() == "X")) {
-                if (Main.buttons[8].getText() == ""){
-                    Main.buttons[8].setFont(new Font("MV Boli", 30));
-                    Main.buttons[8].setText("O");
-                    return 1;
-                }
 
-            } else if ((Main.buttons[0].getText() == "X") && (Main.buttons[8].getText() == "X")) {
-                if (Main.buttons[4].getText() == ""){
-                    Main.buttons[4].setFont(new Font("MV Boli", 30));
-                    Main.buttons[4].setText("O");
-                    return 1;
-                }
-            } else if ((Main.buttons[4].getText() == "X") && (Main.buttons[8].getText() == "X")) {
-                if (Main.buttons[0].getText() == ""){
-                    Main.buttons[0].setFont(new Font("MV Boli", 30));
-                    Main.buttons[0].setText("O");
-                    return 1;
-                }
-            }else {
-                break;
+        if ((Main.buttons[0].getText() == "X") && (Main.buttons[4].getText() == "X")) {
+            if (Main.buttons[8].getText() == ""){
+                Main.buttons[8].setFont(new Font("MV Boli", 30));
+                Main.buttons[8].setText("O");
+                return 1;
+            }
+
+        } else if ((Main.buttons[0].getText() == "X") && (Main.buttons[8].getText() == "X")) {
+            if (Main.buttons[4].getText() == ""){
+                Main.buttons[4].setFont(new Font("MV Boli", 30));
+                Main.buttons[4].setText("O");
+                return 1;
+            }
+        } else if ((Main.buttons[4].getText() == "X") && (Main.buttons[8].getText() == "X")) {
+            if (Main.buttons[0].getText() == ""){
+                Main.buttons[0].setFont(new Font("MV Boli", 30));
+                Main.buttons[0].setText("O");
+                return 1;
             }
         }
 
