83f6b3341fa7d95368d3c7c5404fdf5836f107e3
Sadik-Yasin-Eftee <sadikyasin492@gmail.com>
2021-09-19 22:12:14 +0600

    Refactoring #1

--- src/sample/HighContrastTheme.java
+++ src/sample/HighContrastTheme.java
@@ -10,8 +10,8 @@ public class HighContrastTheme implements ThemeSelection{
     }
     public void setButtonAndBorderColor(){
         for (int i = 0 ; i < 9 ; i++) {
-            Main.buttons[i].setStyle("-fx-border-color: Green");
-            Main.buttons[i].setStyle("-fx-background-color: Green");
+            Main.buttons[i].setStyle("-fx-border-color: Grey");
+            Main.buttons[i].setStyle("-fx-background-color: Grey");
         }
     }
 }
